516b25359de990d7cc9cd29bf8e1e45166399039
DAMNIEL <114008220+Damniel-Guilherme@users.noreply.github.com>
2024-01-08 20:00:32 -0300

    chore: refatorando js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -30,18 +30,3 @@ opcoes.forEach(opcao => {
         imagem.src = opcao.dataset.opcaoImagem;
     });
 });
-
-//Modal
-// document.getElementById('abrirModal').addEventListener('click', function() {
-//     document.getElementById('modal').style.display = 'block';
-//   });
-  
-//   document.getElementsByClassName('fechar')[0].addEventListener('click', function() {
-//     document.getElementById('modal').style.display = 'none';
-//   });
-  
-//   window.addEventListener('click', function(event) {
-//     if (event.target == document.getElementById('modal')) {
-//       document.getElementById('modal').style.display = 'none';
-//     }
-//   });
